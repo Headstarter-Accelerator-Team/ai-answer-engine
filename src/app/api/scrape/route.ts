35d2302f338b26dfd4edf3208ea32c89035aaaa7
Ashan Deen <ash474d@gmail.com>
2025-05-14 19:55:22 -0400

    logo

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import puppeteer from "puppeteer-core";
 
-export async function POST(req: Response) {
+export async function POST(req: NextRequest) {
   console.log("Endpoint hit /api/scrape");
 
   try {
